0c7f12264a7dc338c5d7d9e1f9b6dccfb7e11699
Manpreet <manpreet.parmar246@gmail.com>
2020-05-15 11:13:12 -0700

    Changes to event list page

--- Firebase/public/scripts/events.js
+++ Firebase/public/scripts/events.js
@@ -1,31 +1,15 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyDfdy4eeQA-kzFh5U-6Dl9dsvCxJCiYU2I",
-    authDomain: "team25-52eec.firebaseapp.com",
-    databaseURL: "https://team25-52eec.firebaseio.com",
-    projectId: "team25-52eec",
-    storageBucket: "team25-52eec.appspot.com",
-    messagingSenderId: "744979759825",
-    appId: "1:744979759825:web:fdb78ef312d8ea8370fbf0"
-};
+document.addEventListener("DOMContentLoaded", event => {
+    const app = firebase.app();
+    
+    const db = firebase.firestore();
+    
+    const myPost = db.collection('events').doc('oK79uysjfVsCYZQ8KeON');
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+    myPost.onSnapshot(doc => {
+        const data = doc.data();
 
-getActivities();
-            
-var arr = localStorage.getItem("arr");
-            
-console.log("this is arr: " + arr);
-console.log(arr.length);
-            
-function getActivities() {
-    firebase.auth().onAuthStateChanged(function(user) {
-        db.collection("events").doc(user.uid)
-        .onSnapshot(function(snap) {
-            x = snap.data().arr;
-            console.log(x);
-            localStorage.setItem("arr", x);
-        });
-    });
-}
+        var event = document.getElementById("events");
+        
+        event.write(data.title + '<br>')
+        })
+});
