fd5a294116ef11ec0fc3e501dfbbf0b880abd01f
Manpreet <manpreet.parmar246@gmail.com>
2020-05-21 18:40:29 -0700

    Added schedule page

---
+++ Firebase/public/scripts/schedule.js
@@ -0,0 +1,42 @@
+function showSchedule() {
+    firebase.auth().onAuthStateChanged(function() {
+        let num = 0;
+        
+        db.collection("events").get()
+            .then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                    console.log("in for each");
+                    events = true;
+                    let myDoc = doc.data();
+                    console.log(myDoc);
+
+                    num++;
+
+                    let myHtml = `
+                    <li id="schedule">
+                        <h3><b><u> Event ${num} </u></b></h3>
+                        <h3><b>Name:  </b> ${myDoc.Name} <h3>
+                        <h3><b>Date:  </b> ${myDoc.Date} <h3>
+                        <h3><b>Time:  </b> ${myDoc.Time} </h3>
+                        <h3><b>Duration:  </b> ${myDoc.Duration} </h3>
+                    </li>
+                        `
+
+                    $('#list').append(myHtml);
+
+            });
+        })
+        .then(function() {
+            if(events == false) {
+                console.log("No schedule created yet");
+                $(".container").append('<h3>No schedule created yet</h3>')
+            }
+        })
+        .catch(function(error) {
+            console.log("Error getting documents", error);
+        });
+
+    });
+
+
+}
